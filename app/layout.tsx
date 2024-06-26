0201614167749791bc3b9cf559f2e17d3aae30e0
tragger Emperor <30513005+TraGGERy@users.noreply.github.com>
2024-06-26 11:30:58 -0700

    Update layout.tsx
    define

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import Footer from '@/components/footer'
 import { Sidebar } from '@/components/sidebar'
 import { Toaster } from '@/components/ui/sonner'
 import { ClerkProvider } from "@clerk/nextjs";
+import AdSense from '@/components/AdSense';
 
 const fontSans = FontSans({
   subsets: ['latin'],
