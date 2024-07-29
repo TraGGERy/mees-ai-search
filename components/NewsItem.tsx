3854e1ed183a9e54ab6acfa7d1ad78d294269b3e
TraGGery <traggerozzyboune@gmail.com>
2024-07-29 15:44:44 -0700

    mees ai 2.88

--- components/NewsItem.tsx
+++
@@ -1,8 +0,0 @@
-const NewsItem = ({ title, description, url }) => (
-    <article className={}>
-      <h2><a href={url} target="_blank" rel="noopener noreferrer">{title}</a></h2>
-      <p>{description}</p>
-    </article>
-  );
-  
-  export default NewsItem;
