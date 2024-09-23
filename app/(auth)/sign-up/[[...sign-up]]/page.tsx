538a57270be77104f70de04f03ab980e18e9a5ef
TraGGery <traggerozzyboune@gmail.com>
2024-09-23 16:44:20 -0700

    signup Button

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,8 +1,11 @@
-import { SignUp } from "@clerk/nextjs";
+import { SignIn, SignUp } from "@clerk/nextjs";
+import React from 'react'
 
-export default function Page() {
-  return
-  <div className="flex items-center justify-center h-screen">
-        <SignUp />;
-  </div>
+export default function signUpPage() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+         <SignUp />
+    </div>
+ 
+);
 }
