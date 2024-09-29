0c06d3ebd0f8c01f9317637af748769ecb18d94c
TraGGery <traggerozzyboune@gmail.com>
2024-09-29 09:45:12 -0700

    default ai

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,7 +1,7 @@
 /** @type { import("drizzle-kit").Config } */
 
 export default {
-    schema: "./lib/utils/schema.tsx",
+    schema: "./utils/schema.tsx",
     dialect: 'postgresql',
     dbCredentials: {
       url:"postgresql://athena-ai_owner:Ar0XBvVokyl3@ep-square-waterfall-a5svtxaq.us-east-2.aws.neon.tech/mees-search?sslmode=require",
