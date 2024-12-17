f87dedda8e41890c0dee6b22e3a9f4ae43e9bcd2
TraGGery <traggerozzyboune@gmail.com>
2024-12-16 18:21:38 -0700

    Initial commit: Mees 0.5

--- hooks/use-toast.ts
+++ hooks/use-toast.ts
@@ -155,7 +155,7 @@ function toast({ ...props }: Toast) {
       ...props,
       id,
       open: true,
-      onOpenChange: (open: String) => {
+      onOpenChange: (open: boolean) => {  // Correct type here
         if (!open) dismiss();
       },
     },
