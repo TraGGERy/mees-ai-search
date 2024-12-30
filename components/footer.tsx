a39458e77287f1fe8d61a6910659794f395dc2a0
TraGGery <traggerozzyboune@gmail.com>
2024-12-30 09:49:07 -0700

    Initial commit: Mees 0.chathistory 85

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import { SiAboutdotme, SiAcademia, SiDiscord, SiFacebook, SiGithub, SiInfosys, SiPrivateinternetaccess, SiTwitter, SiWhatsapp, SiX } from 'react-icons/si'
 import { Button } from './ui/button'
 import { Socials } from './xui/socials'
 
 const Footer: React.FC = () => {
+  const pathname = usePathname()
+  
+  if (pathname === '/chat') return null
+  
   return (
     <footer className="w-fit p-0 md:p-2 fixed bottom-0 right-0">
       <div className="flex justify-end">
