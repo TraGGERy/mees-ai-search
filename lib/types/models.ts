4b89677e275b2add64cc0ab23251ab1951917c5b
TraGGery <traggerozzyboune@gmail.com>
2025-01-02 01:32:33 -0700

    Initial commit: Mees 0.chathistory this it gange7

--- lib/types/models.ts
+++ lib/types/models.ts
@@ -6,18 +6,20 @@ export interface Model {
 }
 
 export const models: Model[] = [
-  {
-    id: 'gpt-4o',
-    name: 'GPT-4o',
-    provider: 'OpenAI',
-    providerId: 'openai'
-  },
+  
   {
     id: 'gpt-4o-mini',
     name: 'GPT-4o mini',
     provider: 'OpenAI',
     providerId: 'openai'
   },
+  {
+    id: 'gpt-4o',
+    name: 'GPT-4o',
+    provider: 'OpenAI',
+    providerId: 'openai'
+  },
+  
   {
     id: 'claude-3-5-sonnet-latest',
     name: 'Claude 3.5 Sonnet',
@@ -42,18 +44,7 @@ export const models: Model[] = [
     provider: 'Google Generative AI',
     providerId: 'google'
   },
-  {
-    id: 'llama3-groq-8b-8192-tool-use-preview',
-    name: 'LLama 3 Groq 8B Tool Use',
-    provider: 'Groq',
-    providerId: 'groq'
-  },
-  {
-    id: 'qwen2.5',
-    name: 'Qwen 2.5',
-    provider: 'Ollama',
-    providerId: 'ollama'
-  },
+ 
   {
     id: process.env.NEXT_PUBLIC_AZURE_DEPLOYMENT_NAME || 'undefined',
     name: process.env.NEXT_PUBLIC_AZURE_DEPLOYMENT_NAME || 'Undefined',
