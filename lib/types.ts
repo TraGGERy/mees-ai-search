e30a018f2fb55f9b22df51a5da7dae61b71749e4
TraGGery <traggerozzyboune@gmail.com>
2025-01-12 15:53:57 -0700

    Initial commit: Mees 0.clear display results cool clear reload button when faster loading history

--- lib/types.ts
+++
@@ -1,9 +0,0 @@
-import { Chat } from "@/types/chat";
-
-import { AIMessage } from "./types";
-
-// Add this interface for AI search chats
-export interface AISearchChat extends Chat {
-  type: 'search';
-  messages: AIMessage[];
-} 
