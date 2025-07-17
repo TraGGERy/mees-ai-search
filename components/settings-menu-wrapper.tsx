c2d9997597676195cf293d24c4f3f4e4088bfa77
TraGGery <traggerozzyboune@gmail.com>
2025-07-17 14:04:31 -0700

    x99

--- components/settings-menu-wrapper.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-
-import { useTranslations } from 'next-intl';
-import { SettingsMenu } from './settings-menu';
-
-export function SettingsMenuWrapper() {
-  const t = useTranslations();
-  return <SettingsMenu t={t} />;
-} 
